eb3847898e350f36cad4adf0d10e02f90c6f5b40
Daniel Woods <daniel.woods@objectpartners.com>
2013-11-14 08:39:12 -0600

    wip commit

---
+++ demo4/src/main/avatar/service/src/main.js
@@ -0,0 +1,40 @@
+"use strict";
+
+var avatar = require("org/glassfish/avatar");
+
+var personProvider = new avatar.JPADataProvider(
+  { persistenceUnit: "mem", createTables: "true", entityType: "rest.Person" });
+
+avatar.registerRestService({ url: 'api/person'}, function () {
+  this.$onGet = function (request, response) {
+    var promise = personProvider.$getCollection().then(function(results) {
+      response.$send({ people: results.data });
+    }, function(error) {
+      avatar.log("error");
+      avatar.log(error);
+    });
+  };
+});
+
+avatar.registerRestService({ url: 'api/person/{id}'}, function () {
+  this.$onGet = function(request, response) {
+    personProvider.$get(this.id, function(error, person) {
+      if (!person) {
+        person = {};
+      }
+      response.$send(person);
+    });
+  };
+
+  this.$onPut = function(request, response) {
+    personProvider.$put(this.id, request.data, function(result) {
+      response.$send(result);
+    });
+  };
+
+  this.$onDelete = function(request, response) {
+    itemsProvider.$delete(this.id, function(result) {
+      response.$send(result);
+    });
+  };
+});
